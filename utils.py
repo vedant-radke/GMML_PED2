7ee3fcaef7f6144d411f27c0299a634fe3b1a03e
vedant-radke <vradke1234@gmail.com>
2025-03-22 01:09:18 +0530

    main utils vision corrected

--- utils.py
+++ utils.py
@@ -731,6 +731,171 @@ class PCA():
             x -= torch.tensor(self.mean, dtype=torch.float32)
         return torch.mm(torch.tensor(self.dvt, dtype=torch.float32), x.transpose(0, 1)).transpose(0, 1)
 
-
 def compute_ap(ranks, nres):
     """
+    Computes average precision for given ranked indexes.
+    
+    Arguments
+    ---------
+    ranks : zerro-based ranks of positive images
+    nres  : number of positive images
+    
+    Returns
+    -------
+    ap    : average precision
+    """
+
+    # number of images ranked by the system
+    nimgranks = len(ranks)
+
+    # accumulate trapezoid area
+    ap = 0
+
+    recall_step = 1.0 / nres
+
+    for j in range(nimgranks):
+        rank = ranks[j]
+
+        if rank == 0:
+            precision_0 = 1.0
+        else:
+            precision_0 = float(j) / rank
+
+        precision_1 = float(j + 1) / (rank + 1)
+
+        ap += (precision_0 + precision_1) * recall_step / 2.0
+
+    return ap
+
+
+def compute_map(ranks, gnd, kappas=[]):
+    """
+    Computes the mAP for a given set of returned results.
+         
+    Arguments
+    ---------
+    ranks  : list of lists of ranks of retrieved images for each query
+    gnd    : list of ground truth dictionaries for each query
+    kappas : list of k values for which to compute map, e.g., [1, 5, 10]
+    
+    Returns
+    -------
+    map    : mean average precision
+    """
+    
+    if not kappas:
+        kappas = [1, 5, 10]
+
+    map = 0.
+    for qid in range(len(gnd)):
+        qgnd = gnd[qid]
+        qranks = ranks[qid]
+        
+        ap = compute_ap(qranks, len(qgnd['ok']))
+        map += ap
+        
+    map = map / len(gnd)
+    
+    return map
+
+
+class GaussianBlur(object):
+    """
+    Apply Gaussian Blur to the PIL image.
+    """
+    def __init__(self, p=0.5, radius_min=0.1, radius_max=2.):
+        self.prob = p
+        self.radius_min = radius_min
+        self.radius_max = radius_max
+
+    def __call__(self, img):
+        do_it = random.random() <= self.prob
+        if not do_it:
+            return img
+
+        return img.filter(
+            ImageFilter.GaussianBlur(
+                radius=random.uniform(self.radius_min, self.radius_max)
+            )
+        )
+
+
+class Solarization(object):
+    """
+    Apply Solarization to the PIL image.
+    """
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
+
+
+class DataAugmentationDINO(object):
+    """
+    Data augmentation for DINO training.
+    """
+    def __init__(
+        self,
+        global_crops_scale,
+        local_crops_scale,
+        local_crops_number,
+        global_crops_number=2,
+        augmentation_type='default',
+        color_jitter_scale=1.0,
+    ):
+        flip_and_color_jitter = tf.Compose([
+            tf.RandomHorizontalFlip(p=0.5),
+            tf.RandomApply(
+                [tf.ColorJitter(brightness=0.4*color_jitter_scale, 
+                               contrast=0.4*color_jitter_scale, 
+                               saturation=0.2*color_jitter_scale, 
+                               hue=0.1*color_jitter_scale)],
+                p=0.8
+            ),
+            tf.RandomGrayscale(p=0.2),
+        ])
+        if augmentation_type == 'default':
+            augmentation = [
+                tf.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+                flip_and_color_jitter,
+                GaussianBlur(1.0),
+                tf.ToTensor(),
+                tf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        else:
+            raise ValueError(f"Unknown augmentation type: {augmentation_type}")
+        
+        global_crops_aug = []
+        for i in range(global_crops_number):
+            global_crops_aug.append(tf.Compose(augmentation))
+
+        local_transfo = [
+            tf.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            GaussianBlur(0.5),
+            Solarization(0.2),
+            tf.ToTensor(),
+            tf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+        local_crops_aug = []
+        for i in range(local_crops_number):
+            local_crops_aug.append(tf.Compose(local_transfo))
+            
+        self.global_crops_aug = global_crops_aug
+        self.local_crops_aug = local_crops_aug
+
+    def __call__(self, image):
+        crops = []
+        for op in self.global_crops_aug:
+            crops.append(op(image))
+        for op in self.local_crops_aug:
+            crops.append(op(image))
+        return crops
+
+
+if __name__ == '__main__':
+    pass
